2b170259fc0809d02a302337c25b11d06029ca03
bry4nmachadodev <bryanmachado1703@icloud.com>
2025-12-24 19:35:43 -0300

    Criar método converter parar .txt

--- src/main/java/com/br/bry4n/OpenCSV.java
+++ src/main/java/com/br/bry4n/OpenCSV.java
@@ -1,6 +1,8 @@
 package com.br.bry4n;
 
 import com.opencsv.CSVWriter;
+
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -24,5 +26,30 @@ public class OpenCSV {
         }
 
         return "Arquivo salvo em: " + arquivoCSV.getAbsolutePath();
+    }
+
+    public String escreverTextoTXT(List<String[]> frases, String nomeArquivo){
+        String areaDeTrabalho = System.getProperty("user.home") + File.separator + "Desktop";
+
+        File pastaCacCli = new File(areaDeTrabalho, "cac-cli");
+        if (!pastaCacCli.exists()) {
+            pastaCacCli.mkdir();
+        }
+
+        File arquivoTXT = new File(pastaCacCli, nomeArquivo + ".txt");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(arquivoTXT))) {
+            for (String[] frase : frases) {
+                writer.write("Frente: " + frase[0]);
+                writer.newLine();
+                writer.write("Verso: " + frase[1]);
+                writer.newLine();
+                writer.write("---");
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return "Arquivo salvo em: " + arquivoTXT.getAbsolutePath();
     }
 }
