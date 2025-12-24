00aaa2655a2d6065b266428abe126387d23081f0
bry4nmachadodev <bryanmachado1703@icloud.com>
2025-12-24 18:06:35 -0300

    Criando protótipo menu

--- src/main/java/com/br/bry4n/Main.java
+++ src/main/java/com/br/bry4n/Main.java
@@ -1,7 +1,19 @@
 package com.br.bry4n;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        //declarações
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("##--Seja bem vindo--##\n\n");
+        System.out.print("|-----------------------------|\n");
+        System.out.print("| Opção 1 - Converter para .txt|\n");
+        System.out.print("| Opção 2 - Converter para .csv|\n");
+        System.out.print("|-----------------------------|\n");
+        System.out.print("Digite uma opção: ");
+
+        int escolha = 0;
     }
 }
