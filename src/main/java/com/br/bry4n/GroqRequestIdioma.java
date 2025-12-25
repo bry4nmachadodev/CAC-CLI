964aa9c936f7451e9bd7fff4cf88142966ecd782
bry4nmachadodev <bryanmachado1703@icloud.com>
2025-12-25 11:41:06 -0300

    Método retonar Array de String

--- src/main/java/com/br/bry4n/GroqRequestIdioma.java
+++ src/main/java/com/br/bry4n/GroqRequestIdioma.java
@@ -1,19 +1,27 @@
 package com.br.bry4n;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroqRequestIdioma {
 
-    public String requestAoGroq(String idioma, String nivel, String numeroFrases){
-        String prompt = "Monte pra mim um deck com " + numeroFrases + " estilo chunks com as palavras mais necessárias e utéis baseadas no nível " + nivel + "do idioma: " + idioma;
+    public List<String[]> requestAoGroq(String idioma, String nivel, String numeroFrases){
+        String prompt = "Me de " + numeroFrases + " frases pra adicionar no anki do nível " + nivel + " do idioma: " + idioma + " . Me de somente as frases, não precisa cumprimentar nem nada." + " Juntamente as frases colocque a tradução dela pra eu adicionar no verso no anki. Exemplo: palavranoidioma;tradução";
         String resposta = null;
         try {
             resposta = GroqAPI.chamarGroq(prompt);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        System.out.println(resposta);
-        return resposta;
+
+        List<String[]> deck = new ArrayList<>();
+        for (String linha : resposta.split("\n")) {
+            deck.add(linha.split(";")); // cada linha vira [frente, verso]
+        }
+
+        return deck;
     }
 
 }
+// Me de 2 frass pra adicionar no anki do nível A1 do idioma: russo . Me de somente [...]
