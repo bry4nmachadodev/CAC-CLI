fba4a811ec08235032b76398b7a5dcd835513c86
bry4nmachadodev <bryanmachado1703@icloud.com>
2025-12-26 18:21:05 -0300

    Adicionar +1 opção ao menu

--- src/main/java/com/br/bry4n/main/Menu.java
+++ src/main/java/com/br/bry4n/main/Menu.java
@@ -6,12 +6,13 @@ public class Menu {
 
     public void exibirMenu(String username) {
         System.out.print(" -- Seja bem vindo " + username + " --\n");
-        System.out.print("| ----------------------------- |\n");
-        System.out.print("| Opção 1 - Converter para .txt |\n");
-        System.out.print("| Opção 2 - Converter para .csv |\n");
-        System.out.print("| Opção 3 - IA - gerar deck     |\n");
-        System.out.print("| Opção 4 - Sair                |\n");
-        System.out.print("| ----------------------------- |\n");
+        System.out.print("| --------------------------------- |\n");
+        System.out.print("|  Opção 1 - Converter para .txt    |\n");
+        System.out.print("|  Opção 2 - Converter para .csv    |\n");
+        System.out.print("|  Opção 3 - IA - gerar deck        |\n");
+        System.out.print("|  Opção 4 - Crie seu deck colorido |\n");
+        System.out.print("|  Opção 5 - Sair                   |\n");
+        System.out.print("| --------------------------------- |\n");
     }
 
     public int capturarOpcao(Scanner scanner) {
